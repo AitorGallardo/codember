1b339d77b9167ce2ecd57ba85dd6c2a521b8e589
Aitor Gallardo <38296940+AitorGallardo@users.noreply.github.com>
2022-11-27 02:02:06 +0100

    fancier fourthy

--- src/challenge04/index.js
+++ src/challenge04/index.js
@@ -1,25 +1,19 @@
 const firstNumber = 11098;
 const lastNumber = 98123;
-let validNumbers = [];
+const validNumbers = [];
 
 for (let currentNumber = firstNumber; currentNumber < lastNumber; currentNumber++) {
 
-  let digits = currentNumber.toString().split('');
-  let realDigits = digits.map(Number);
+  let realDigits = currentNumber.toString().split('').map(Number);
 
-  const hasAscendingNumbers =  realDigits.every((_, index) => {
-    if (realDigits[index + 1] !== undefined)return realDigits[index] <= realDigits[index + 1];
-
-    return true;
-  });
-
+  const hasAscendingNumbers = realDigits.every((_, index) => index === realDigits.length-1 ? true : realDigits[index] <= realDigits[index + 1]);
 
   if(!hasAscendingNumbers) continue;
     
   const hasNum5 = realDigits.filter((digit)=> digit === 5).length >= 2;
 
   if(hasNum5){
-    validNumbers.push(currentNumber)
+    validNumbers.push(currentNumber);
   }
 }
 console.dir(validNumbers, {'maxArrayLength': null});
